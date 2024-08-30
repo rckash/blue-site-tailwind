8d03a80d9190e28191bed7bf2decb6ffb2cd088e
RC <rckashyapcpe@gmail.com>
2024-08-30 12:07:07 +0800

    Update stylesheet href

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}", "./src/blocks/content-center.html"],
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
       fontFamily: {
