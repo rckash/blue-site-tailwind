c134e68c67a70890f0bd4f8bba657f5c5cb4d7d2
RC <rckashyapcpe@gmail.com>
2024-08-30 11:56:14 +0800

    Update bg-image src

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}", "./src/blocks/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js}", "./src/blocks/content-center.html"],
   theme: {
     extend: {
       fontFamily: {
